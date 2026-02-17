2357e83ad9368ec289e61c9ca62d80f3ccc90d7b
Dmytro yawuxi@proton.me Rylov <yawuxi@proton.me>
2026-02-17 20:32:39 +0100

    refactor&feature: dropped echo, pause feature

--- pomodoro-i3.sh
+++ pomodoro-i3.sh
@@ -16,28 +16,43 @@ fi
 . "$pomodoro_config_file_path";
 
 color=$work_color;
-work_time_seconds=$(( $work_time * 60 ));
-short_brake_seconds=$(( $short_brake * 60 ));
-long_brake_seconds=$(( $long_brake * 60 ));
+work_time_seconds=$(( $work_time ));
+short_brake_seconds=$(( $short_brake ));
+long_brake_seconds=$(( $long_brake ));
 
 # Retrieve current timestamp
 function get_current_time() {
-  echo $(date +%s);
+  date +%s;
 }
 
-# Retrieve total cycle counts
-function get_cycle_counts() {
-  echo $(awk -F = 'NR==3 {print $2}' "$pomodoro_data_tmp_file_path");
+#Is pomodoro timer started
+function is_started() {
+  echo $(( $(awk -F = 'NR==1 {print $2}' "$pomodoro_data_tmp_file_path") && 1));
 }
 
 # Retrieve cycle start timestamp
 function get_current_cycle_start_time() {
-  echo $(awk -F = 'NR==1 {print $2}' "$pomodoro_data_tmp_file_path");
+  awk -F = 'NR==2 {print $2}' "$pomodoro_data_tmp_file_path";
 }
 
 # Retrieve cycle end timestamp
 function get_current_cycle_end_time() {
-  echo $(awk -F = 'NR==2 {print $2}' "$pomodoro_data_tmp_file_path");
+  awk -F = 'NR==3 {print $2}' "$pomodoro_data_tmp_file_path";
+}
+
+#Is pomodoro timer paused
+function is_paused() {
+  echo $(( $(awk -F = 'NR==4 {print $2}' "$pomodoro_data_tmp_file_path") && 1));
+}
+
+#Retrieve pause timestamp
+function get_pause_time() {
+  awk -F = 'NR==5 {print $2}' "$pomodoro_data_tmp_file_path";
+}
+
+#Is pomodoro timer started
+function is_short_brake() {
+  awk -F = 'NR==6 {print $2}' "$pomodoro_data_tmp_file_path";
 }
 
 # Retrieve cycle short brake end timestamp
@@ -45,72 +60,87 @@ function get_current_cycle_short_brake_end_time() {
   echo $(( $(get_current_cycle_end_time) + "$short_brake_seconds" ));
 }
 
+#Is pomodoro timer long brake activated
+function is_long_brake() {
+  awk -F = 'NR==7 {print $2}' "$pomodoro_data_tmp_file_path";
+}
+
 # Retrieve cycle long brake end timestamp
 function get_current_cycle_long_brake_end_time() {
   echo $(( $(get_current_cycle_end_time) + "$long_brake_seconds" ));
 }
 
-#Is pomodoro timer started
-function is_started() {
-  echo $(( $(get_current_cycle_start_time) && 1));
-}
-
-#Is pomodoro timer started
-function is_short_brake() {
-  echo $(awk -F = 'NR==4 {print $2}' "$pomodoro_data_tmp_file_path");
-}
-
-#Is pomodoro timer long brake activated
-function is_long_brake() {
-  echo $(awk -F = 'NR==5 {print $2}' "$pomodoro_data_tmp_file_path");
+# Retrieve total cycle counts
+function get_cycle_counts() {
+  awk -F = 'NR==8 {print $2}' "$pomodoro_data_tmp_file_path";
 }
 
 #Listening to mouse events
 case "${BLOCK_BUTTON:-0}" in
   # Starting timer
   1) {
-    if [[ $(get_current_cycle_start_time) -eq 0 && $(get_current_cycle_end_time) -eq 0 && $(get_cycle_counts) -eq 0 ]]; then
+    if [[ $(get_current_cycle_start_time) -eq 0 && $(get_current_cycle_end_time) -eq 0 && $(get_cycle_counts) -eq 0 && $(is_paused) -eq 0 ]]; then
+      sed -i "s/is_started=[0-9]\+/is_started=1/" "$pomodoro_data_tmp_file_path";
       sed -i "s/start_time=[0-9]\+/start_time=$(get_current_time)/" "$pomodoro_data_tmp_file_path";
       sed -i "s/end_time=[0-9]\+/end_time=$(( $(get_current_time) + $work_time_seconds ))/" "$pomodoro_data_tmp_file_path";
+    fi
+
+    if [[ $(is_paused) -eq 1 ]]; then
+      pause_duration=$(( $(get_current_time) - $(get_pause_time) ));
+      sed -i "s/is_paused=[0-9]\+/is_paused=0/" "$pomodoro_data_tmp_file_path";
+      sed -i "s/pause_time=[0-9]\+/pause_time=0/" "$pomodoro_data_tmp_file_path";
+      sed -i "s/is_started=[0-9]\+/is_started=1/" "$pomodoro_data_tmp_file_path";
+      sed -i "s/end_time=[0-9]\+/end_time=$(( $(get_current_cycle_end_time) + $pause_duration ))/" "$pomodoro_data_tmp_file_path";
+    fi
+  };;
+  # Pausing/Unpausing timer
+  2) {
+    if [[ $(is_paused) -eq 0 ]]; then
+      sed -i "s/is_paused=[0-9]\+/is_paused=1/" "$pomodoro_data_tmp_file_path";
+      sed -i "s/pause_time=[0-9]\+/pause_time=$(get_current_time)/" "$pomodoro_data_tmp_file_path";
+      sed -i "s/is_started=[0-9]\+/is_started=0/" "$pomodoro_data_tmp_file_path";
     fi
   };;
   # Stopping timer
   3) {
+      sed -i "s/is_started=[0-9]\+/is_started=0/" "$pomodoro_data_tmp_file_path";
       sed -i "s/start_time=[0-9]\+/start_time=0/" "$pomodoro_data_tmp_file_path";
       sed -i "s/end_time=[0-9]\+/end_time=0/" "$pomodoro_data_tmp_file_path";
+      sed -i "s/is_paused=[0-9]\+/is_paused=0/" "$pomodoro_data_tmp_file_path";
+      sed -i "s/pause_time=[0-9]\+/pause_time=0/" "$pomodoro_data_tmp_file_path";
       sed -i "s/cycle_count=[0-9]\+/cycle_count=0/" "$pomodoro_data_tmp_file_path";
-      sed -i "s/short_brake=[0-9]\+/short_brake=0/" "$pomodoro_data_tmp_file_path";
-      sed -i "s/long_brake=[0-9]\+/long_brake=0/" "$pomodoro_data_tmp_file_path";
+      sed -i "s/is_short_brake=[0-9]\+/is_short_brake=0/" "$pomodoro_data_tmp_file_path";
+      sed -i "s/is_long_brake=[0-9]\+/is_long_brake=0/" "$pomodoro_data_tmp_file_path";
   };;
 esac
 
 # Cycle end check
-if [[ $(get_current_time) -eq $(get_current_cycle_end_time) ]]; then
+if [[ $(get_current_time) -eq $(get_current_cycle_end_time) && $(is_paused) -eq 0 ]]; then
   sed -i "s/cycle_count=[0-9]\+/cycle_count=$(( $(get_cycle_counts) + 1 ))/" "$pomodoro_data_tmp_file_path";
   sed -i "s/start_time=[0-9]\+/start_time=0/" "$pomodoro_data_tmp_file_path";
 
   if [[ $(( $(get_cycle_counts) % "$cycle_counts" )) -gt 0 ]]; then
-    sed -i "s/short_brake=0\+/short_brake=1/" "$pomodoro_data_tmp_file_path";
+    sed -i "s/is_short_brake=0\+/is_short_brake=1/" "$pomodoro_data_tmp_file_path";
     notify-send "$short_brake_start_message";
   fi
 
   if [[ $(( $(get_cycle_counts) % "$cycle_counts" )) -eq 0 ]]; then
-    sed -i "s/long_brake=0\+/long_brake=1/" "$pomodoro_data_tmp_file_path";
+    sed -i "s/is_long_brake=0\+/is_long_brake=1/" "$pomodoro_data_tmp_file_path";
     notify-send "$long_brake_start_message";
   fi
 fi
 
 #End of short brake, and starting new cycle
-if [[ ( $(( $(get_current_cycle_short_brake_end_time) - $(get_current_time) )) -eq 0 ) && ( $(is_short_brake) -eq 1 ) ]]; then
-  sed -i "s/short_brake=1\+/short_brake=0/" "$pomodoro_data_tmp_file_path";
+if [[ ( $(( $(get_current_cycle_short_brake_end_time) - $(get_current_time) )) -eq 0 ) && ( $(is_short_brake) -eq 1 ) && ( $(is_paused) -eq 0 ) ]]; then
+  sed -i "s/is_short_brake=1\+/is_short_brake=0/" "$pomodoro_data_tmp_file_path";
   sed -i "s/start_time=[0-9]\+/start_time=$(get_current_time)/" "$pomodoro_data_tmp_file_path";
   sed -i "s/end_time=[0-9]\+/end_time=$(( $(get_current_time) + $work_time_seconds ))/" "$pomodoro_data_tmp_file_path";
   notify-send "$short_brake_end_message";
 fi
 
 #End of long brake, and starting new cycle
-if [[ ( $(( $(get_current_cycle_long_brake_end_time) - $(get_current_time) )) -eq 0 ) && ( $(is_long_brake) -eq 1 ) ]]; then
-  sed -i "s/long_brake=1\+/long_brake=0/" "$pomodoro_data_tmp_file_path";
+if [[ ( $(( $(get_current_cycle_long_brake_end_time) - $(get_current_time) )) -eq 0 ) && ( $(is_long_brake) -eq 1 ) && ( $(is_paused) -eq 0 ) ]]; then
+  sed -i "s/is_long_brake=1\+/is_long_brake=0/" "$pomodoro_data_tmp_file_path";
   sed -i "s/start_time=[0-9]\+/start_time=$(get_current_time)/" "$pomodoro_data_tmp_file_path";
   sed -i "s/end_time=[0-9]\+/end_time=$(( $(get_current_time) + $work_time_seconds ))/" "$pomodoro_data_tmp_file_path";
   notify-send "$long_brake_end_message";
@@ -152,6 +182,27 @@ if [[ $(is_long_brake) -eq 1 ]]; then
 
   color="$long_brake_color";
   output=$(printf "$long_brake_label %02d:%02d:%02d" \
+  "${remaining_hours%.*}" \
+  "${remaining_minutes%.*}" \
+  "${remaining_seconds%.*}")
+fi
+
+if [[ $(is_paused) -eq 1 ]]; then
+  seconds=$(( $(get_current_cycle_end_time) - $(get_pause_time) ));
+
+  if [[ $(is_short_brake) -eq 1 ]]; then
+    seconds=$(( $(get_current_cycle_short_brake_end_time) - $(get_pause_time) ));
+  fi
+
+  if [[ $(is_long_brake) -eq 1 ]]; then
+    seconds=$(( $(get_current_cycle_long_brake_end_time) - $(get_pause_time) ));
+  fi
+
+  remaining_hours=$(( "$seconds" / 60 / 60));
+  remaining_minutes=$(( ( "$seconds" % 3600 ) / 60 ));
+  remaining_seconds=$(( "$seconds" % 60 ));
+
+  output=$(printf "$pause_label %02d:%02d:%02d" \
   "${remaining_hours%.*}" \
   "${remaining_minutes%.*}" \
   "${remaining_seconds%.*}")
